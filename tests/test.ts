a100cb1ee6deebc6927a198b3ea0ad881a345f17
Jhuliana <jholeanasantos@gmail.com>
2022-09-21 18:25:56 -0300

    feat: upvote by id of recommendation

--- tests/test.ts
+++ tests/test.ts
@@ -24,43 +24,7 @@ describe("ROUTE RECOMMENDATIONS", () => {
         expect(result.status).toEqual(200)
         expect(existRecommendation.score).toBeLessThan(updateRecoomendation.score)
     });
-    it('POST POST /recommendations/:id/downvote', async () => {
-        const recommendation = await recomendationRandom()
-        const existRecommendation = await createRecommendation(recommendation)
-        const result = await supertest(app).post(`/recommendations/${existRecommendation.id}/downvote`)
-        const updateRecoomendation = await verifyExitsRecommendation(existRecommendation.name)
-        expect(result.status).toEqual(200)
-        expect(updateRecoomendation.score).toBeLessThan(existRecommendation.score)
-    })
-    it('GET /recommendations', async () => {
-        const recommendations = await manyRecomendationRandom()
-        const existRecommendation = await insertMany(recommendations)
-        const result = await supertest(app).get('/recommendations')
-        const totalLength = existRecommendation.count
-        expect(result.body).toHaveLength(totalLength)
-    })
-    it('GET /recommendations/:id', async () => {
-        const recommendation = await recomendationRandom()
-        const existRecommendation = await createRecommendation(recommendation)
-        const result = await supertest(app).get(`/recommendations/${existRecommendation.id}`)
-        expect(result.body).not.toBeFalsy()
-        expect(result.body).toBeInstanceOf(Object)
-        expect(result.body).toMatchObject(existRecommendation)
-    })
-    it('GET /recommendations/random', async () => {
-        const recommendations = await manyRecomendationRandom()
-        await insertMany(recommendations)
-        const result = await supertest(app).get('/recommendations/random')
-        const existRecommendation = await verifyExitsRecommendation(result.body.name)
-        expect(result.body).not.toBeFalsy()
-        expect(result.body).toBeInstanceOf(Object)
-        expect(existRecommendation).not.toBeFalsy()
-    })
-    // it('GET /recommendations/top/:amount', async () => {
-    //     const recommendations = await manyRecomendationRandom()
-    //     await insertMany(recommendations)
 
-    // })
 })
 
 afterAll(async () => {
